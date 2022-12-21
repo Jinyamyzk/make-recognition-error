1409733803c53e3feef730661fa636bd01429b04
Jinyamyzk <63580543+Jinyamyzk@users.noreply.github.com>
2022-12-21 17:30:11 +0900

    Chnage num epoch to 5

--- train.py
+++ train.py
@@ -197,7 +197,7 @@ def main():
     criterion = FairBCELoss()
 
     # 学習・検証を実行する
-    num_epochs = 1
+    num_epochs = 5
     net_trained = train_model(net, dataloaders_dict,
                             criterion, optimizer, num_epochs=num_epochs)
 

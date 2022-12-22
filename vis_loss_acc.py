04f9d27736601ab78e27efeb7ae4249614acc96c
Jinyamyzk <63580543+Jinyamyzk@users.noreply.github.com>
2022-12-22 18:12:24 +0900

    m

--- vis_loss_acc.py
+++ vis_loss_acc.py
@@ -2,6 +2,6 @@ import pickle
 
 with open('loss_acc.pickle', 'rb') as f:
     loss_acc = pickle.load(f)
-loss_acc["train_acc"] = [acc.tolist()[0] for acc in loss_acc["train_acc"]]
-loss_acc["valid_acc"] = [acc.tolist()[0] for acc in loss_acc["valid_acc"]]
+loss_acc["train_acc"] = [acc.tolist() for acc in loss_acc["train_acc"]]
+loss_acc["valid_acc"] = [acc.tolist() for acc in loss_acc["valid_acc"]]
 print(loss_acc)
